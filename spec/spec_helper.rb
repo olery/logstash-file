7e384530abf9bda797ed7db87d8e547314fbf9f0
Yorick Peterse <yorickpeterse@olery.com>
2014-06-02 13:55:56 +0200

    Upgrade to RSpec 3.0.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,12 @@ require_relative '../lib/logstash-file'
 
 RSpec.configure do |config|
   config.color = true
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
